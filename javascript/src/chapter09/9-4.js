d2214533437ed5986ad67e9e1aa4234a6fdb843c
woong9149 <sewoong0409@naver.com>
2023-06-11 16:49:07 +0900

    230611 refactoring 2nd chapter09_2

---
+++ javascript/src/chapter09/9-4.js
@@ -0,0 +1,62 @@
+// 예시
+// class Person
+// {
+//   constructor() {
+//     this._telephoneNumber = new TelephoneNumber();
+//   }
+//
+//   get officeAreaCode() {return this._telephoneNumber.areaCode;}
+//   set officeAreaCode(arg) {this._telephoneNumber.areaCode = arg;}
+//   get officeNumber() { return this._telephoneNumber.number;}
+//   set officeNumber(arg) {this._telephoneNumber.number = arg;}
+//
+// }
+//
+// class TelephoneNumber
+// {
+//   get areaCode() {return this._areaCode;}
+//   set areaCode(arg) {this._areaCode = arg;}
+//   get number() {return this._number;}
+//   set number(arg) { this._number = arg;}
+// }
+
+class Person
+{
+  constructor() {
+    this._telephoneNumber = new TelephoneNumber();
+  }
+
+  get officeAreaCode() {return this._telephoneNumber.areaCode;}
+  set officeAreaCode(arg) {
+    this._telephoneNumber.areaCode = new TelephoneNumber(arg, this.officeNumber);
+  }
+  get officeNumber() { return this._telephoneNumber.number;}
+  set officeNumber(arg) {
+    this._telephoneNumber.number = new TelephoneNumber(this.officeAreaCode, arg);
+  }
+
+}
+
+class TelephoneNumber
+{
+  constructor(areaCode, number) {
+    this._areaCode = areaCode;
+    this._number = number;
+  }
+
+  get areaCode() {return this._areaCode;}
+  set areaCode(arg) {this._areaCode = arg;}
+  get number() {return this._number;}
+  set number(arg) { this._number = arg;}
+
+  equals(other) {
+    if(!(other instanceof  TelephoneNumber)) return false;
+    return this.areaCode === other.areaCode &&
+      this.number === other.number;
+  }
+}
+
+if('telephone equals', function() {
+  assert(new TelephoneNumber("312", "555-0142")
+    .equals(new TelephoneNumber("312", "555-0142")));
+});
---
+++ javascript/src/chapter09/9-6.js
@@ -0,0 +1,9 @@
+// 예시
+// function potentialEnergy(mass, height) {
+//   return mass * 9.81 * height;
+// }
+
+const STANDARD_GRAVITY = 9.81;
+function potentialEnergy(mass, height) {
+  return mass * STANDARD_GRAVITY * height;
+}
